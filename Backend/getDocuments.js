844731e073e7ca4a1f3d744edd47911879c23763
Spark10000 <kevinhaller26@gmail.com>
2022-08-23 19:26:53 -0400

    Querry Folders First

--- Backend/getDocuments.js
+++ Backend/getDocuments.js
@@ -4,31 +4,42 @@
 var onshape = require('./Perif/onshape.js');
 var queryObject = {};
 
- var getDocuments = function (queryObject) {
-  
-  getDocuments = function(queryObject, cb) {
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+  var getDocuments = function(queryObject, cb) {
     var opts = {
-      path: '/api/documents/',//?nodeId=474a5b1d281c06c556b810b5/'
+      path: '/api/documents/'+'?nodeId=4e8628e24c7fb6ed3907920a&resourceType=folder',
       query: queryObject,
       headers: {
-      'Authorization': 'Bearer ' + 'EISZOODI34HKXKFRQKBKFPWXOLSR5NBBGR2BIAQ=' //need to get a user accesstoken // request.AccessToken
+      'Authorization': 'Bearer ' + 'V5DYX5D746QX4JIXUXUXIFD7JOV4WVQ6PDNZGJOYNLO7AVF3D5CA====' //need to get a user accesstoken // request.AccessToken
       }
     }
     onshape.get(opts, cb);
   }
-  getDocuments(queryObject, function (data) {
-    console.log(data);
-    var docs = JSON.parse(data.toString()).items;  
-    for (var i = 0; i < docs.length; i++) {
+
+    getDocuments(queryObject, function (data) {
+      console.log(data);
+    });
+
+  /*getDocuments(queryObject, function (data) {
+    //console.log(data);
+    var docs = JSON.parse(data.toString()).items; 
+
+    for (var i = 0; i < data.length; i++) {
       var docID = docs[i].id;
       //what ever the image url is 
       //var privacy = docs[i].public ? 'public' : 'private';
       //var ownerName = (docs[i].owner && ('name' in docs[i].owner)) ? docs[i].owner.name : 'nobody';
       //if(ownerName == 'FTC2901 Administrator' && privacy == 'public'){
       console.log(docID + '    ' + docs[i].name);
+     // if(i % 19 == 0){
+     //   sleep(61000);
+      //}
       }
-    //}
   });
-};
-
-getDocuments(queryObject);
